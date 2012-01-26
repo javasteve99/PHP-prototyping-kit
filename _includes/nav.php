41aa4bbd535ccf8a1e8e412a30aa3ddb1429ab56
Steve Hickey <steve.hickey@freshtilledsoil.com>
2012-01-26 08:38:25 -0500

    Setting up test for title and body class variables

---
+++ _includes/nav.php
@@ -0,0 +1,7 @@
+<nav>
+  <ul>
+    <li><a href="index.php" class="home">Home</a></li>
+    <li><a href="page-2.php" class="page-2">Page 2</a></li>
+    <li><a href="page-3.php" class="page-3">Page 3</a></li>
+  </ul>
+</nav>
